a972bdee3ae82dffa3aa6070cb6c7dcfab01a585
PotmanNob <Potmannob@outlook.com>
2024-06-28 17:06:13 -0500

    added drive code

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,71 +5,93 @@
 package frc.robot;
 
 import java.io.File;
-import edu.wpi.first.wpilibj.Filesystem;
+import frc.robot.Constants;
 
+
+import edu.wpi.first.math.geometry.Translation2d;
+import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
 import swervelib.SwerveDrive;
+import swervelib.math.SwerveMath;
 import swervelib.parser.SwerveParser;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+* The VM is configured to automatically run this class, and to call the functions corresponding to
+* each mode, as described in the TimedRobot documentation. If you change the name of this class or
+* the package after creating this project, you must also update the build.gradle file in the
+* project.
+*/
 public class Robot extends TimedRobot {
-  /**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+/**
+* This function is run when the robot is first started up and should be used for any
+* initialization code.
+*/
 
-   //initalize xbox controller
-   XboxController controller = new XboxController(0);
+//initalize xbox controller
+    XboxController controller = new XboxController(0);
 
-   SwerveDrive swerveDrive;
+    boolean isFieldRelative = true;
 
-  @Override
-  public void robotInit() {
-    try {
-      File swerveParserFile = new File(Filesystem.getDeployDirectory(), "swerve/neokraken");
-      SwerveParser swerveParser = new SwerveParser(swerveParserFile);
-      swerveDrive = swerveParser.createSwerveDrive(40); // random num
-    } catch (Exception e) {
-      System.out.println(e);
+    SwerveDrive swerveDrive;
+
+    @Override
+    public void robotInit() {
+        try {
+            File swerveParserFile = new File(Filesystem.getDeployDirectory(), "swerve/neokraken");
+            SwerveParser swerveParser = new SwerveParser(swerveParserFile);
+
+            double driveConversionFactor = SwerveMath.calculateMetersPerRotation(Constants.Drive.wheelDiameter, Constants.Drive.driveGearRatio, 1);
+            double angleConversionFactor = SwerveMath.calculateDegreesPerSteeringRotation(Constants.Drive.angleGearRatio, 1);
+
+            swerveDrive = swerveParser.createSwerveDrive(Constants.Drive.maxSpeed, angleConversionFactor, driveConversionFactor); // random num
+        } catch (Exception e) {
+            System.out.println(e);
+        }
     }
-  }
 
-  @Override
-  public void robotPeriodic() {}
+    @Override
+    public void robotPeriodic() {}
 
-  @Override
-  public void autonomousInit() {}
+    @Override
+    public void autonomousInit() {}
 
-  @Override
-  public void autonomousPeriodic() {}
+    @Override
+    public void autonomousPeriodic() {}
 
-  @Override
-  public void teleopInit() {}
+    @Override
+    public void teleopInit() {}
 
-  @Override
-  public void teleopPeriodic() {}
+    @Override
+    public void teleopPeriodic() {
+        if (controller.getAButtonPressed())
+        {
+            isFieldRelative = isFieldRelative ? false : true;
+        }
 
-  @Override
-  public void disabledInit() {}
+        //controls are amplified
+        double yMovement = controller.getLeftY() * -3; //inversed y movement
+        double xMovement = controller.getLeftX() * 3;
+        double rotation = controller.getRightX() * 2;
 
-  @Override
-  public void disabledPeriodic() {}
+        swerveDrive.drive(new Translation2d(xMovement, yMovement), rotation, isFieldRelative, false);
+    }
 
-  @Override
-  public void testInit() {}
+    @Override
+    public void disabledInit() {}
 
-  @Override
-  public void testPeriodic() {}
+    @Override
+    public void disabledPeriodic() {}
 
-  @Override
-  public void simulationInit() {}
+    @Override
+    public void testInit() {}
 
-  @Override
-  public void simulationPeriodic() {}
+    @Override
+    public void testPeriodic() {}
+
+    @Override
+    public void simulationInit() {}
+
+    @Override
+    public void simulationPeriodic() {}
 }
